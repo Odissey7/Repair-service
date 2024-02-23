0dd79f5ea2b31a1f1d0e6b03676e07abf32f88b6
Odissey7 <agapidu@inbox.ru>
2024-02-23 14:08:16 +0300

    fin

---
+++ js/indexHead.js
@@ -0,0 +1,38 @@
+// POPUP
+
+document.querySelector(".btn_popup_1").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_2").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_3").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_4").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_5").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_6").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_7").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_8").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_9").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_10").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_11").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+document.querySelector(".btn_popup_12").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
